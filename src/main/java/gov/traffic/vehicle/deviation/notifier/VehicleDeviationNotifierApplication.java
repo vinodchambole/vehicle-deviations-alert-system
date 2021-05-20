f7d3b54a3c12c2ab07e5d67d5eb5779d08ed4abb
s9wgco <kumar.vinod@siemens.com>
2021-05-20 23:06:09 +0530

    First commit

---
+++ src/main/java/gov/traffic/vehicle/deviation/notifier/VehicleDeviationNotifierApplication.java
@@ -0,0 +1,13 @@
+package gov.traffic.vehicle.deviation.notifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleDeviationNotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleDeviationNotifierApplication.class, args);
+	}
+
+}
