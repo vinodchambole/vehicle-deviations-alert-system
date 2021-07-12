14a5c65e74f11f73ba229505b13cf72330913e3c
Vinod <37335021+vchambole@users.noreply.github.com>
2021-07-12 21:32:13 +0530

    Update VehicleEntityDeviationNotifierApplicationTests.java

--- src/test/java/gov/traffic/vehicle/deviation/notifier/VehicleEntityDeviationNotifierApplicationTests.java
+++ src/test/java/gov/traffic/vehicle/deviation/notifier/VehicleEntityDeviationNotifierApplicationTests.java
@@ -1,9 +1,11 @@
 package gov.traffic.vehicle.deviation.notifier;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class VehicleEntityDeviationNotifierApplicationTests {
 
 	@Test
